86556697f6028cdb74455a400d70784dafa499c9
AHONYO Israel <izy.clapp@gmail.com>
2023-06-23 14:22:37 +0000

    first commit

---
+++ parallax.js
@@ -0,0 +1,48 @@
+gsap.from("#l",{
+    scrollTrigger: {
+        scrub: true
+    },
+
+    x: 600,
+})
+
+gsap.from("#nav",{
+    scrollTrigger: {
+        scrub: true
+    },
+
+    x: 700,
+})
+
+gsap.from("#m",{
+    scrollTrigger: {
+        scrub: true
+    },
+
+    x: -1000,
+    y :1000,
+})
+
+gsap.from(".mini2",{
+    scrollTrigger: {
+        scrub: true
+    },
+
+    y: 700,
+})
+
+gsap.from(".mini1",{
+    scrollTrigger: {
+        scrub: true
+    },
+
+    y:- 700,
+})
+
+gsap.from(".mini",{
+    scrollTrigger: {
+        scrub: true
+    },
+
+    x: 1200,
+})
